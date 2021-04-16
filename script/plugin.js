be02505b9d596f7a47b0715d5bf930d4fd8d0b32
Yeseo Lee <s2019w35@e-mirim.hs.kr>
2021-04-16 17:42:03 +0900

    the code submit

---
+++ script/plugin.js
@@ -0,0 +1,12 @@
+var input = document.getElementById("input");
+var story = document.getElementById("story");
+
+function AnswerSubmitted()
+{
+  if( input.value == "the code."){
+    story.innerHTML = 
+    "내가 문을 열었던가? 기억하는 한";
+  } else {
+    alert("the code: code error!");
+  }
+}
