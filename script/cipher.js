afb76dda260405e05e20ed6b1162d5feb98c1803
Yeseo Lee <s2019w35@e-mirim.hs.kr>
2021-04-23 15:41:37 +0900

    html 우선 구현 완성

--- script/cipher.js
+++ script/cipher.js
@@ -1,37 +1,25 @@
-//@author Jonathan Walker
-//@version 1.0
-//@license http://creativecommons.org/licenses/by-sa/4.0/
-// Playfair Cipher - Javascript Implementation
-
-// Create our cipher object that we will store our data in
 jQuery.cipher = {
-  key: "", // Initialize a blank key.
-  alpha: "", // Stores our alphabet, used for making the key table.
-  allowed: "ABCDEFGHIKLMNOPQRSTUVWXYZ", // A master of our alphabet - this will not
-  maxRow: 5, // Rows in the key table. Playfair specifies 5.
-  maxCol: 5, // Columns in the key table. Playfair specifies 5.
-  nullCh: "X", // Char used to break up duplicate letters and fill uneven pairs.
-  randomTable: false, // Randomize the rest of the table? Playfair does not.
+  key: "",
+  alpha: "",
+  allowed: "ABCDEFGHIJKLMNOPQRSTUVWXY",
+  maxRow: 5,
+  maxCol: 5,
+  nullCh: "X",
+  randomTable: false,
   subCh: {
-    sub: "J", // Letter to replace
-    rpl: "I", // Letter to take its place
+    sub: "Z",
+    rpl: "Q",
   },
 };
 
-// Fisher-Yates Shuffle
-// Modified for string handling
-// From: http://bost.ocks.org/mike/shuffle/
 function shuffleStr(str) {
   var array = str.split("");
   var m = array.length,
     t,
     i;
 
-  // While there remain elements to shuffle…
   while (m) {
-    // Pick a remaining element…
     i = Math.floor(Math.random() * m--);
-    // And swap it with the current element.
     t = array[m];
     array[m] = array[i];
     array[i] = t;
@@ -39,7 +27,6 @@ function shuffleStr(str) {
   return array.join("");
 }
 
-// HTML Table for our key table.
 function printKey() {
   var tableHtml = "<table>";
   for (var i = 0; i < 25; i = i + 5) {
@@ -55,7 +42,6 @@ function printKey() {
   $("#keyTable").html(tableHtml);
 }
 
-// Fetches the position of a specific character in the table
 function getCharPosition(c) {
   var index = $.cipher.key.indexOf(c);
   var row = Math.floor(index / 5);
@@ -66,48 +52,40 @@ function getCharPosition(c) {
   };
 }
 
-// Fetches a character based on the given position
-// Position must be an object with both row and col attributes.
 function getCharFromPosition(pos) {
   var index = pos.row * 5;
   index = index + pos.col;
   return $.cipher.key.charAt(index);
 }
 
-// Applies the Playfair rules to a given set of letters.
 function encipherPair(str) {
   if (str.length != 2) return false;
   var pos1 = getCharPosition(str.charAt(0));
   var pos2 = getCharPosition(str.charAt(1));
   var char1 = "";
 
-  // Same Column - Increment 1 row, wrap around to top
   if (pos1.col == pos2.col) {
     pos1.row++;
     pos2.row++;
     if (pos1.row > $.cipher.maxRow - 1) pos1.row = 0;
     if (pos2.row > $.cipher.maxRow - 1) pos2.row = 0;
-    char1 = getCharFromPosition(pos1) + getCharFromPosition(pos2);
+    char1 = getCharFromPosition(pos2) + getCharFromPosition(pos1);
   } else if (pos1.row == pos2.row) {
-    // Same Row - Increment 1 column, wrap around to left
     pos1.col++;
     pos2.col++;
     if (pos1.col > $.cipher.maxCol - 1) pos1.col = 0;
     if (pos2.col > $.cipher.maxCol - 1) pos2.col = 0;
-    char1 = getCharFromPosition(pos1) + getCharFromPosition(pos2);
+    char1 = getCharFromPosition(pos2) + getCharFromPosition(pos1);
   } else {
-    // Box rule, use the opposing corners
     var col1 = pos1.col;
     var col2 = pos2.col;
     pos1.col = col2;
     pos2.col = col1;
-    char1 = getCharFromPosition(pos1) + getCharFromPosition(pos2);
+    char1 = getCharFromPosition(pos2) + getCharFromPosition(pos1);
   }
   return char1;
 }
 
-// Loops a digraph and passes each letter pair to encipherPair
-// Returns the cipher in an array
 function encipher(digraph) {
   if (!digraph) return false;
   var cipher = [];
@@ -117,40 +95,34 @@ function encipher(digraph) {
   return cipher;
 }
 
-// Applies the Playfair rules in reverse to decipher a letter pair
 function decipherPair(str) {
   if (str.length != 2) return false;
   var pos1 = getCharPosition(str.charAt(0));
   var pos2 = getCharPosition(str.charAt(1));
   var char1 = "";
 
-  // Same Column - Decrement 1 row, wrap around to bottom
   if (pos1.col == pos2.col) {
     pos1.row--;
     pos2.row--;
     if (pos1.row < 0) pos1.row = $.cipher.maxRow - 1;
     if (pos2.row < 0) pos2.row = $.cipher.maxRow - 1;
-    char1 = getCharFromPosition(pos1) + getCharFromPosition(pos2);
+    char1 = getCharFromPosition(pos2) + getCharFromPosition(pos1);
   } else if (pos1.row == pos2.row) {
-    // Same row - Decrement 1 column, wrap around to right
     pos1.col--;
     pos2.col--;
     if (pos1.col < 0) pos1.col = $.cipher.maxCol - 1;
     if (pos2.col < 0) pos2.col = $.cipher.maxCol - 1;
-    char1 = getCharFromPosition(pos1) + getCharFromPosition(pos2);
+    char1 = getCharFromPosition(pos2) + getCharFromPosition(pos1);
   } else {
-    // Box rules, use opposing corners (same as forward)
     var col1 = pos1.col;
     var col2 = pos2.col;
     pos1.col = col2;
     pos2.col = col1;
-    char1 = getCharFromPosition(pos1) + getCharFromPosition(pos2);
+    char1 = getCharFromPosition(pos2) + getCharFromPosition(pos1);
   }
   return char1;
 }
 
-// Loops a digraph and passes each letter pair to decipherPair
-// Returns the plaintext in an array
 function decipher(digraph) {
   if (!digraph) return false;
   var plaintext = [];
@@ -160,8 +132,6 @@ function decipher(digraph) {
   return plaintext;
 }
 
-// Turns a string into a digraph
-// Sanitizes the string, returns the digraph in an array
 function makeDigraph(str) {
   if (!str) return false;
   var digraph = [];
@@ -180,21 +150,16 @@ function makeDigraph(str) {
   return digraph;
 }
 
-// Creates our key table based upon a given key string
-// Sanitizes the key string, using a default if one is not provided.
 function generateKeyTable(keystring) {
   if (!keystring) keystring = "PLAYFAIRCIPHER";
 
-  // Sanitize
   keystring = keystring.toUpperCase();
   keystring = keystring.replace(/\W+/g, "");
   keystring = keystring.replace($.cipher.subCh.sub, $.cipher.subCh.rpl);
 
-  // Reset our key and alphabet
   $.cipher.key = "";
   $.cipher.alpha = $.cipher.allowed;
 
-  // Create the start of the table with our key string
   var keyArr = keystring.split("");
   $.each(keyArr, function (x, c) {
     if ($.cipher.alpha.indexOf(c) > -1 && $.cipher.key.indexOf(c) == -1) {
@@ -203,15 +168,10 @@ function generateKeyTable(keystring) {
     }
   });
 
-  // Fill in the rest of the table
-  // If we enabled randomizing the table, do it. Playfair does not.
   if ($.cipher.randomTable) $.cipher.key += shuffleStr($.cipher.alpha);
   else $.cipher.key += $.cipher.alpha;
 }
 
-// Handle Events
-
-// Generates the table
 $("#generateKeytable").click(function () {
   $(this).hide();
   $("#regenerateKeytable").show();
@@ -221,7 +181,6 @@ $("#generateKeytable").click(function () {
   $("#AfterGen").slideDown();
 });
 
-// Regenerates the table
 $("#regenerateKeytable").click(function () {
   $("#AfterGen").hide();
   generateKeyTable($("#keyword").val());
@@ -230,7 +189,6 @@ $("#regenerateKeytable").click(function () {
   $("#AfterGen").slideDown();
 });
 
-// Encipher the contents of the textarea
 $("#encipher").click(function () {
   var digraph = makeDigraph($("#en").val());
   if (!digraph) alert("Bad entry");
@@ -239,7 +197,6 @@ $("#encipher").click(function () {
   $("#de").val(cipher.join(" "));
 });
 
-// Deciphers the contents of the textarea
 $("#decipher").click(function () {
   var digraph = makeDigraph($("#de").val());
   if (!digraph) alert("Bad entry");
--- script/plugin.js
+++ script/plugin.js
@@ -1,10 +1,40 @@
 var input = document.getElementById("input");
-var story = document.getElementById("story");
+var code = document.getElementById("code");
+var key = document.getElementById("key");
+var answerChk = false;
 
-function AnswerSubmitted() {
-  if (input.value == "the code.") {
-    story.innerHTML = "내가 문을 열었던가? 기억하는 한";
+function enterkey() {
+  if (window.event.keyCode == 13) {
+    AnswerSubmitted();
+  }
+}
+
+https: function AnswerSubmitted() {
+  if (input.value == "rem" || input.value == "REM") {
+    title.innerHTML = "Stage 2. 신";
+    key.innerHTML = "key : is";
+    story1.innerHTML =
+      "내가 문을 열었던가?<br>기억하는 한 해석한 글자를 입으로 내뱉었을 뿐인데, 여긴 전혀 다른 공간이다.<br>여전히 어두운 건 마찬가지다. 왜인지 주변이 어그러지는 것 같기도 하고.";
+    code.innerHTML = "AJDHCPCBPG";
+    story2.innerHTML =
+      "또 알 수 없는 단어다. 이번에도 지나왔던 문처럼 해석하면 되려나.";
+    answerChk = true;
+    updateStory();
   } else {
     alert("the code: code error!");
+  }
+}
+
+https: function AnswerSubmitted1() {
+  if (input.value == "lightsaber" || input.value == "LIGHTSABER") {
+    title.innerHTML = "Stage 3. 의무감";
+    key.innerHTML = "key : coming";
+    story1.innerHTML =
+      "역시나. 이번에도 다른 공간이다.<br>내가 무슨 단어를 말했는지조차 기억이 나지 않는 이곳을 어떻게 설명해야 할까?<br>애초에 나는 왜 나가기 위해 노력하고 있을까?";
+    code.innerHTML = "JVQEIANVCWBGYI";
+    story2.innerHTML = "해석해야 한다.";
+    AnswerSubmitted1();
+  } else if (answerChk == false) {
+    alert("the code: code ㅇㄹㄹㅎㄴㄹㅎㅁerror!");
   }
 }
